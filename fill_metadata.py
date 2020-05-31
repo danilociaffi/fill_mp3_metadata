f06caa3c54023d89504f06d7afe9404292a11c22
danilociaffi <danilo.ciaffi@gmail.com>
2020-06-01 00:15:34 +0200

    Initial commit

--- fill_metadata.py
+++ fill_metadata.py
@@ -1,11 +1,9 @@
 import os, eyed3, csv, tkinter as tk
 from shutil import copyfile
-from eyed3 import id3
 from tkinter import messagebox
 
 def get_cover_path():
     cover_path = "./Input/Artwork"
-
     try:
         return cover_path + "/" + os.listdir(cover_path)[0]
     except:
@@ -13,14 +11,12 @@ def get_cover_path():
 
 def import_metadata():
     with open("metadata.csv") as file:
-
         reader = csv.DictReader(file)
 
         meta = {}
         for row in reader:
             key = row.pop('title')
             meta[key] = row
-        
     return meta
 
 def check_filenames(in_path, meta):
@@ -40,7 +36,6 @@ def check_filenames(in_path, meta):
         return True
 
 def set_meta():
-
     in_path = "./Input/Tracks" 
     out_path = "./Output" 
 
@@ -56,17 +51,8 @@ def set_meta():
 
             # Grab tag values from csv
             title = track.split(" [")[0]
-
             tmp = meta[title]
-            
-            artist = tmp["artist"] 
-            album_artist = tmp["album_artist"]
-            genre = tmp["genre"]
-            composer = tmp["composer"]
-            album = tmp["album"]
-            disc_num = tmp["disc_num"]
-            track_num = tmp["track_num"]
-            year = tmp["year"]
+
             # Load MP3
             mp3 = eyed3.load(in_path + '/' + track) 
             
@@ -75,25 +61,25 @@ def set_meta():
                 mp3.initTag() 
 
             # Set the tags
-            mp3.tag.artist = artist 
-            mp3.tag.album_artist = album_artist
-            mp3.tag.non_std_genre = genre
             mp3.tag.title = title
-            mp3.tag.composer = composer
-            mp3.tag.album = album
+            mp3.tag.artist = tmp["artist"]  
+            mp3.tag.album_artist = tmp["album_artist"]
+            mp3.tag.non_std_genre = tmp["genre"]
+            mp3.tag.composer = tmp["composer"]
+            mp3.tag.album = tmp["album"]
             mp3.tag.images.set(3, open(cover_path,'rb').read(), 'image/png')
-            mp3.tag.year = year
+            mp3.tag.year = tmp["year"]
 
-            if disc_num:
-                mp3.tag.disc_num = disc_num
-            if track_num:
-                mp3.tag.track_num = track_num
+            if tmp["disc_num"]:
+                mp3.tag.disc_num = tmp["disc_num"]
+            if tmp["track_num"]:
+                mp3.tag.track_num = tmp["track_num"]
     
             # Save tags
             mp3.tag.save()    
 
             # Save a renamed copy to the Output folder
-            copyfile(in_path + '/' + track, out_path + '/' + title + " [" + artist + "].mp3")
+            copyfile(in_path + '/' + track, out_path + '/' + title + " [" + tmp["artist"] + "].mp3")
         
         tk.messagebox.showinfo(title="Done!", message="All files have been processed")
 
@@ -117,7 +103,6 @@ lbox = tk.Listbox(window, yscrollcommand = scrollbar.set)
 lbox.config(width=400, height=300)
 
 scrollbar.config(command = lbox.yview)
-
 
 if __name__ == "__main__":
     window.mainloop()
